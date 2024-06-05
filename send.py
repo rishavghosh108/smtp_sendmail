ca64bc123684a32e59dbf8f78e5c7fdc3e1a976b
rishavghosh147 <rishavghosh147@gmail.com>
2024-06-05 13:17:52 +0530

    test

--- send.py
+++ send.py
@@ -5,24 +5,21 @@ from smtpd import SMTPServer
 
 class EmlServer(SMTPServer):
     no = 0
-    def process_message(self, peer, mailfrom, rcpttos, data):
-        filename = '%s-%d.eml' % (datetime.now().strftime('%Y%m%d%H%M%S'),
-                self.no)
-        f = open(filename, 'w')
-        f.write(data)
-        f.close
+
+    def process_message(self, peer, mailfrom, rcpttos, data, **kwargs):
+        filename = '%s-%d.eml' % (datetime.now().strftime('%Y%m%d%H%M%S'), self.no)
+        with open(filename, 'w') as f:
+            f.write(data)
         print('%s saved.' % filename)
         self.no += 1
 
-
 def run():
-    # start the smtp server on localhost:1025
+    # start the smtp server on localhost:25
     foo = EmlServer(('0.0.0.0', 25), None)
     try:
         asyncore.loop()
     except KeyboardInterrupt:
         pass
-
 
 if __name__ == '__main__':
     run()
